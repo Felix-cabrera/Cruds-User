c8a21f5a07b13b90def2f8b5fb798aafb852b31a
Felix Cabrera <141293650+Felix-cabrera@users.noreply.github.com>
2023-12-12 18:59:38 -0500

    change today

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   const [infoUpdate, setInfoUpdate] = useState()
 
-  const baseUrl = 'https://user-backend-dev-hrnn.3.us-1.fl0.io'
+  const baseUrl = 'https://user-backend-dev-sfrc.1.us-1.fl0.io'
   const [users, getUsers, createUser, deleteUser, updateUser ] = useFetch(baseUrl)
 
   useEffect(() => {
